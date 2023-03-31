e6d642e1448188c4bfa6efeffce95b0208c92a2e
Hunny Garg <hghunny.garg12@gmail.com>
2023-03-31 16:45:46 +0530

    added message in diff.java

--- add.java
+++ add.java
@@ -3,6 +3,6 @@ class add
     public static void main(String args[])
     {
         int a=5,b=6;
-        System.out.pritnln("sum of two numbers" +(a+b));
+        System.out.pritnln(a+b);
     }
 }
